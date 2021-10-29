714416272bcbc239f5b9985cae03c87d28d39a52
Jonathan <joas4713@student.dsv.se>
2021-10-30 01:50:30 +0200

    files filled with temp values

--- src/main/java/se/joas/mysoftware/redditbot/Bot.java
+++ src/main/java/se/joas/mysoftware/redditbot/Bot.java
@@ -7,16 +7,47 @@ import net.dean.jraw.http.UserAgent;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Bot {
 
-    public void readCredentialsFromFile() {
+    public static void main(String[] args) {
+        Bot bot = new Bot();
 
+        bot.auth(bot.readUserAgentFromFile(), bot.readCredentialsFromFile());
     }
 
-    public void run(String platform, String appId, String version, String redditUsername,
-                    String username, String password, String clientId, String clientSecret) {
-        UserAgent userAgent = new UserAgent(platform, appId, version, redditUsername);
-        Credentials credentials = Credentials.script(username, password, clientId, clientSecret);
+    public String[] readCredentialsFromFile() {
+        try {
+            FileReader fileReader = new FileReader("src/main/java/se/joas/mysoftware/redditbot/credentials.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            return line.split(",");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String[0];
+    }
+
+    public String[] readUserAgentFromFile() {
+        try {
+            FileReader fileReader = new FileReader("src/main/java/se/joas/mysoftware/redditbot/userAgent.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            return line.split(",");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String[0];
+    }
+
+    public void auth(String[] userAgents, String[] credential) {
+        UserAgent userAgent = new UserAgent(userAgents[0], userAgents[1], userAgents[2], userAgents[3]);
+        Credentials credentials = Credentials.script(credential[0], credential[1], credential[2], credential[3]);
         NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
         RedditClient redditClient = OAuthHelper.automatic(adapter, credentials);
     }
--- src/test/java/se/joas/mysoftware/redditbot/BotTest.java
+++ src/test/java/se/joas/mysoftware/redditbot/BotTest.java
@@ -6,11 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BotTest {
 
+    String[] credentials = new String[] {"username", "password", "clientId", "clientSecret"};
+
     Bot bot = new Bot();
 
     @Test
     void redditLoginCompletes() {
         //assertTrue(bot.login());
+    }
+
+    @Test
+    void readsCredentialsFromFile() {
+        String[] actual = bot.readCredentialsFromFile();
+        assertEquals(credentials[0], actual[0]);
+        assertEquals(credentials[1], actual[1]);
+        assertEquals(credentials[2], actual[2]);
+        assertEquals(credentials[3], actual[3]);
     }
 
 }
