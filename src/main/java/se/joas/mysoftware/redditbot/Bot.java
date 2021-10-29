ea6a36848c4d1615e208100bacfc8a8db86b526b
Jonathan <joas4713@student.dsv.se>
2021-10-30 00:08:00 +0200

    Initial commit

---
+++ src/main/java/se/joas/mysoftware/redditbot/Bot.java
@@ -0,0 +1,4 @@
+package se.joas.mysoftware.redditbot;
+
+public class Bot {
+}
---
+++ src/test/java/se/joas/mysoftware/redditbot/BotTest.java
@@ -0,0 +1,16 @@
+package se.joas.mysoftware.redditbot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BotTest {
+
+    Bot bot = new Bot();
+
+    @Test
+    void redditLoginCompletes() {
+        //assertTrue(bot.login());
+    }
+
+}
