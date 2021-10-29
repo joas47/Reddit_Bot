663e35c9526de2f277f02140c6d276d214353b1e
Jonathan <joas4713@student.dsv.se>
2021-10-30 01:55:46 +0200

    test

--- src/test/java/se/joas/mysoftware/redditbot/BotTest.java
+++ src/test/java/se/joas/mysoftware/redditbot/BotTest.java
@@ -2,8 +2,6 @@ package se.joas.mysoftware.redditbot;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BotTest {
 
     String[] credentials = new String[] {"username", "password", "clientId", "clientSecret"};
@@ -13,15 +11,6 @@ class BotTest {
     @Test
     void redditLoginCompletes() {
         //assertTrue(bot.login());
-    }
-
-    @Test
-    void readsCredentialsFromFile() {
-        String[] actual = bot.readCredentialsFromFile();
-        assertEquals(credentials[0], actual[0]);
-        assertEquals(credentials[1], actual[1]);
-        assertEquals(credentials[2], actual[2]);
-        assertEquals(credentials[3], actual[3]);
     }
 
 }
