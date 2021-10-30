9ec2a390a1c903166e31a8e7aec0983e96b224d3
Jonathan <joas4713@student.dsv.se>
2021-10-30 18:33:25 +0200

    methods for making a self post and setting user flair.

--- src/main/java/se/joas/mysoftware/redditbot/Bot.java
+++ src/main/java/se/joas/mysoftware/redditbot/Bot.java
@@ -4,19 +4,46 @@ import net.dean.jraw.RedditClient;
 import net.dean.jraw.http.NetworkAdapter;
 import net.dean.jraw.http.OkHttpNetworkAdapter;
 import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.models.Flair;
+import net.dean.jraw.models.SubmissionKind;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
+import net.dean.jraw.references.SubredditReference;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class Bot {
 
+    private static RedditClient redditClient;
+
     public static void main(String[] args) {
         Bot bot = new Bot();
-
         bot.auth(bot.readUserAgentFromFile(), bot.readCredentialsFromFile());
+
+    }
+
+    private void makeSelfPost(String subredditName, String title, String content) {
+        SubredditReference subredditReference = redditClient.subreddit(subredditName);
+        subredditReference.submit(SubmissionKind.SELF, title, content, false);
+    }
+
+    // Sets user flair to the first item in the list
+    private void setUserFlair(String subredditName) {
+        SubredditReference subreddit = redditClient.subreddit(subredditName);
+        List<Flair> userFlairOptions = subreddit.userFlairOptions();
+        if (!userFlairOptions.isEmpty()) {
+            Flair newFlair = userFlairOptions.get(0);
+            subreddit.selfUserFlair().updateToTemplate(newFlair.getId(), "");
+        }
+    }
+
+    // TODO:
+    private void commentInSubreddit(SubredditReference subredditReference, String threadId) {
+        redditClient.comment("mm5xu9");
+        //RootCommentNode  root = redditClient.submission(threadId).comments();
     }
 
     public String[] readCredentialsFromFile() {
@@ -49,7 +76,7 @@ public class Bot {
         UserAgent userAgent = new UserAgent(userAgents[0], userAgents[1], userAgents[2], userAgents[3]);
         Credentials credentials = Credentials.script(credential[0], credential[1], credential[2], credential[3]);
         NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
-        RedditClient redditClient = OAuthHelper.automatic(adapter, credentials);
+        redditClient = OAuthHelper.automatic(adapter, credentials);
     }
 
 
