7a9fb5ef08f145029c85c845a7c237bf60392ffc
Jonathan <joas4713@student.dsv.se>
2021-10-31 22:33:25 +0100

    methods for making comment replies, comment deletion

--- src/main/java/se/joas/mysoftware/redditbot/Bot.java
+++ src/main/java/se/joas/mysoftware/redditbot/Bot.java
@@ -4,15 +4,20 @@ import net.dean.jraw.RedditClient;
 import net.dean.jraw.http.NetworkAdapter;
 import net.dean.jraw.http.OkHttpNetworkAdapter;
 import net.dean.jraw.http.UserAgent;
-import net.dean.jraw.models.Flair;
-import net.dean.jraw.models.SubmissionKind;
+import net.dean.jraw.models.*;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
+import net.dean.jraw.pagination.DefaultPaginator;
+import net.dean.jraw.references.CommentReference;
+import net.dean.jraw.references.SubmissionReference;
 import net.dean.jraw.references.SubredditReference;
+import net.dean.jraw.tree.CommentNode;
+import net.dean.jraw.tree.RootCommentNode;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 public class Bot {
@@ -23,6 +28,55 @@ public class Bot {
         Bot bot = new Bot();
         bot.auth(bot.readUserAgentFromFile(), bot.readCredentialsFromFile());
 
+    }
+
+    // Maybe useful in future
+    private void findThreadInSubredditBasedOnTitle(String subredditName, String postTitle) {
+        SubredditReference subreddit = redditClient.subreddit(subredditName);
+        DefaultPaginator<Submission> subredditNew = subreddit.posts().sorting(SubredditSort.NEW).limit(10).build();
+        Listing<Submission> submissions = subredditNew.next();
+        String submissionId = "";
+        for (Submission s : submissions) {
+            if (s.getTitle().equals(postTitle)) {
+                submissionId = s.getId();
+            }
+        }
+    }
+
+    private void makeCommentReplyInThread(String threadId, String commentId, String reply) {
+        RootCommentNode rootCommentNode = redditClient.submission(threadId).comments();
+        Iterator<CommentNode<PublicContribution<?>>> it = rootCommentNode.walkTree().iterator();
+        while (it.hasNext()) {
+            PublicContribution<?> thing = it.next().getSubject();
+            if (thing instanceof Comment && thing.getId().equals(commentId)) {
+                CommentReference commentReference = new CommentReference(redditClient, thing.getId());
+                commentReference.reply(reply);
+            }
+        }
+    }
+
+    private void deleteCommentInThread(String threadId) {
+        RootCommentNode rootCommentNode = redditClient.submission(threadId).comments();
+        Iterator<CommentNode<PublicContribution<?>>> it = rootCommentNode.walkTree().iterator();
+        while (it.hasNext()) {
+            PublicContribution<?> thing = it.next().getSubject();
+            if (thing instanceof Comment) {
+                CommentReference commentReference = new CommentReference(redditClient, thing.getId());
+                commentReference.delete();
+            }
+        }
+    }
+
+    private void makeParentCommentInThread(String threadId, String comment) {
+        RootCommentNode rootCommentNode = redditClient.submission(threadId).comments();
+        Iterator<CommentNode<PublicContribution<?>>> it = rootCommentNode.walkTree().iterator();
+        while (it.hasNext()) {
+            PublicContribution<?> thing = it.next().getSubject();
+            if (thing instanceof Submission) {
+                SubmissionReference submissionReference = new SubmissionReference(redditClient, thing.getId());
+                submissionReference.reply(comment);
+            }
+        }
     }
 
     private void makeSelfPost(String subredditName, String title, String content) {
@@ -40,10 +94,15 @@ public class Bot {
         }
     }
 
-    // TODO:
-    private void commentInSubreddit(SubredditReference subredditReference, String threadId) {
-        redditClient.comment("mm5xu9");
-        //RootCommentNode  root = redditClient.submission(threadId).comments();
+    private void listCommentsInThread(String threadId) {
+        RootCommentNode rootCommentNode = redditClient.submission(threadId).comments();
+        Iterator<CommentNode<PublicContribution<?>>> it = rootCommentNode.walkTree().iterator();
+        while (it.hasNext()) {
+            PublicContribution<?> thing = it.next().getSubject();
+            if (thing instanceof Comment) {
+                System.out.println(thing.getBody());
+            }
+        }
     }
 
     public String[] readCredentialsFromFile() {
